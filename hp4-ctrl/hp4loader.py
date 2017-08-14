5dbd26089b6160290dd08a1a7bc55cfb50e0cd21
David Hancock <dhancock@cs.utah.edu>
2017-08-14 03:33:10 -0600

    Implement most of hp4loader::load; start RuleTranslationGuide
    Instantiation of RuleTranslationGuide keeps rule translation
    templates in memory in each virtual device.  Methods
    developed in previous versions have been identified.

--- hp4-ctrl/hp4loader.py
+++ hp4-ctrl/hp4loader.py
@@ -9,6 +9,100 @@ class HP4Loader():
     compiled_programs = {} # {program_path (str) : cr (hp4compiler.CodeRepresentation)}
     hp4c = hp4compiler.P4_to_HP4()
 
+  def finish(self, object_code_path, vdev_ID):
+    f_ac = open(object_code_path, 'r')
+    code = []
+
+    sr = {}
+    sr['[vdev ID]'] = vdev_ID
+    sr['[PROCEED]'] = '0'
+    sr['[PARSE_SELECT_SEB]'] = '1'
+    sr['[PARSE_SELECT_20_29]'] = '2'
+    sr['[PARSE_SELECT_30_39]'] = '3'
+    sr['[PARSE_SELECT_40_49]'] = '4'
+    sr['[PARSE_SELECT_50_59]'] = '5'
+    sr['[PARSE_SELECT_60_69]'] = '6'
+    sr['[PARSE_SELECT_70_79]'] = '7'
+    sr['[PARSE_SELECT_80_89]'] = '8'
+    sr['[PARSE_SELECT_90_99]'] = '9'
+    sr['[EXTRACT_MORE]'] ='10'
+    sr['[DONE]'] = '0'
+    sr['[EXTRACTED_EXACT]'] = '1'
+    sr['[METADATA_EXACT]'] = '2'
+    sr['[STDMETA_EXACT]'] = '3'
+    sr['[EXTRACTED_VALID]'] = '4'
+    sr['[STDMETA_INGRESS_PORT_EXACT]'] = '5'
+    sr['[STDMETA_PACKET_LENGTH_EXACT]'] = '6'
+    sr['[STDMETA_INSTANCE_TYPE_EXACT]'] = '7'
+    sr['[STDMETA_EGRESS_SPEC_EXACT]'] =	'8'
+    sr['[MATCHLESS]'] = '99'
+    sr['[COMPLETE]'] = '1'
+    sr['[CONTINUE]'] = '2'
+    sr['[MODIFY_FIELD]'] = '0'
+    sr['[ADD_HEADER]'] = '1'
+    sr['[COPY_HEADER]'] = '2'
+    sr['[REMOVE_HEADER]'] = '3'
+    sr['[MODIFY_FIELD_WITH_HBO]'] = '4'
+    sr['[TRUNCATE]'] = '5'
+    sr['[DROP]'] = '6'
+    sr['[NO_OP]'] = '7'
+    sr['[PUSH]'] = '8'
+    sr['[POP]'] = '9'
+    sr['[COUNT]'] = '10'
+    sr['[METER]'] = '11'
+    sr['[GENERATE_DIGEST]'] = '12'
+    sr['[RECIRCULATE]'] = '13'
+    sr['[RESUBMIT]'] = '14'
+    sr['[CLONE_INGRESS_INGRESS]'] = '15'
+    sr['[CLONE_EGRESS_INGRESS]'] = '16'
+    sr['[CLONE_INGRESS_EGRESS]'] = '17'
+    sr['[CLONE_EGRESS_EGRESS]'] = '18'
+    sr['[MULTICAST]'] = '19'
+    sr['[MATH_ON_FIELD]'] = '20'
+
+    found_sr = False
+
+    for line in f_ac:
+      if line == '# SEARCH AND REPLACE\n':
+        found_sr = True
+        break
+
+    if(found_sr):
+      line = f_ac.next()
+      while line != '\n':
+        linetoks = line.split()
+        sr[linetoks[1]] = linetoks[3]
+        line = f_ac.next()
+
+    f_ac.seek(0)
+
+    for line in f_ac:
+      # strip out comments and white space
+      if line[0] == '#' or line[0] == '\n':
+        continue
+      i = line.find('#')
+      if i != -1:
+        line = line[0:i]
+        while line.endswith(' '):
+          line = line[0:-1]
+        line += '\n'
+
+      for key in sr.keys():
+        line = line.replace(key, sr[key])
+      for token in re.findall("\[.*?\]", line):
+        replace = ""
+        if re.search("\[[0-9]*x00s\]", token):
+          numzeros = int(re.search("[0-9]+", token).group())
+          for i in range(numzeros):
+            replace += "00"   
+          line = line.replace(token, replace)
+
+      code.append(line)
+
+    f_ac.close()
+
+    return code
+
   def load(self, vdev_name, vdev_ID, program_path):
     if program_path not in compiled_programs:
       # compile
@@ -20,12 +114,38 @@ class HP4Loader():
       else:
         raise CompileError('filetype not supported')
     
-    vdev_ID, self.compiled_programs[program_path].object_code_path
+    object_code_path = self.compiled_programs[program_path].object_code_path
+    rtg_path = self.compiled_programs[program_path].rule_translation_guide_path
+   
+    code = finish(object_code_path, vdev_ID)
+    guide = RuleTranslationGuide(rtg_path) # TODO: verify parameters
 
-    if call(["./hp4l", "--input", hp4tf, "--output", finst_name+'.hp4',
-             "--progID", str(self.devices[device].next_PID)) != 0:
-    
-    #code = 
-    #guide = 
-    #return VirtualDevice(vdev_ID, code, guide)
+    return VirtualDevice(vdev_ID, code, guide)
+
+  def writefile(self, program_path, outfile):
     pass
+
+def loader(args):
+  hp4loader = HP4Loader()
+  hp4loader.load(args.output, args.vdevID, args.input)
+  hp4loader.writefile(args.input, args.output)
+
+def parse_args(args):
+  parser = argparse.ArgumentParser(description='HP4 Annotated Commands Converter')
+  parser.add_argument('--input', help='Annotated hp4 commands file',
+                      type=str, action="store", required=True)
+  parser.add_argument('--output', help='Where to write hp4-ready commands file',
+                      type=str, action="store", required=True)
+  parser.add_argument('--vdevID', help='Virtual Device ID',
+                      type=str, action="store", default='1')
+
+  parser.set_defaults(func=loader)
+
+  return parser.parse_args()
+
+def main():
+  args = parse_args(sys.argv[1:])
+  args.func(args)
+
+if __name__ == '__main__':
+  main()
--- hp4-ctrl/hp4translator.py
+++ hp4-ctrl/hp4translator.py
@@ -1,7 +1,13 @@
 import virtualdevice
+import json
 from p4command import P4Command
 
 class Translator():
   @staticmethod
   def translate(self, vdev, p4command):
+    # key method: ~/hp4-src/p4c-hp4/controller.py::DPMUServer::translate
     pass
+
+class RuleTranslationGuide():
+  def __init__(self, rtg_path):
+    # key method: ~/hp4-src/p4c-hp4/controller.py::DPMUServer::parse_json
