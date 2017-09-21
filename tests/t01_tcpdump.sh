74d49fe8fc690152c4a977427e2eeac60b2a486a
David Hancock <dhancock@cs.utah.edu>
2017-09-21 09:01:32 -0600

    Update test scripts
    Mininet version 2.2.1 doesn't crash (more recent versions do)

--- tests/t01_tcpdump.sh
+++ tests/t01_tcpdump.sh
@@ -8,5 +8,16 @@ if [ $# -eq 1 ]
 fi
 
 sudo tcpdump -i s1-eth1 -n -s 0 -w t01_s1_eth1_run$RUN.dump &
+PID_1=$!
 sudo tcpdump -i s1-eth2 -n -s 0 -w t01_s1_eth2_run$RUN.dump &
+PID_2=$!
 sudo tcpdump -i s1-eth3 -n -s 0 -w t01_s1_eth3_run$RUN.dump &
+PID_3=$!
+
+read -n 1 -s
+
+kill -15 $PID_1
+sleep 1
+kill -15 $PID_2
+sleep 1
+kill -15 $PID_3
