f53fc944001db6956570a6ffd105df913db3d1fd
David Hancock <dhancock@cs.utah.edu>
2017-09-21 19:33:49 -0600

    Fix compare_pcaps to examine all interfaces
    L3 router compares perfectly, hp4 vs native

--- tests/compare_pcaps.sh
+++ tests/compare_pcaps.sh
@@ -3,6 +3,7 @@
 TEST=0
 RUN1=0
 RUN2=1
+IFACES=3
 
 if [ $# -gt 0 ]
 then
@@ -12,18 +13,22 @@ then
       RUN1=$2
       if [ $# -gt 2 ]
         then
-        RUN2=$3
+          RUN2=$3
+          if [ $# -gt 3 ]
+            then
+              IFACES=$4
+          fi
       fi
   fi
 fi
 
-f1pre=t${TEST}"_s1_eth1_run"${RUN1}
-f2pre=t${TEST}"_s1_eth1_run"${RUN2}
-
-tcpdump -r $f1pre".dump" -t -vvv -e -n > $f1pre".txt"
-tcpdump -r $f2pre".dump" -t -vvv -e -n > $f2pre".txt"
-
-./process_pcap.py $f1pre".txt" > $f1pre"_processed.txt"
-./process_pcap.py $f2pre".txt" > $f2pre"_processed.txt"
-
-diff $f1pre"_processed.txt" $f2pre"_processed.txt"
+for i in `seq 1 $IFACES`;
+do
+  f1pre=t${TEST}"_s1_eth"$i"_run"${RUN1}
+  f2pre=t${TEST}"_s1_eth"$i"_run"${RUN2}
+  tcpdump -r $f1pre".dump" -t -vvv -e -n > $f1pre".txt"
+  tcpdump -r $f2pre".dump" -t -vvv -e -n > $f2pre".txt"
+  ./process_pcap.py $f1pre".txt" > $f1pre"_processed.txt"
+  ./process_pcap.py $f2pre".txt" > $f2pre"_processed.txt"
+  diff $f1pre"_processed.txt" $f2pre"_processed.txt"
+done
