4fec7ded790b93062b7ac1469afffe280a4baccd
David Hancock <dhancock@cs.utah.edu>
2017-09-26 10:19:56 -0600

    Update t03 and related scripts

--- tests/gen_pairpings.py
+++ tests/gen_pairpings.py
@@ -7,13 +7,27 @@ parser = argparse.ArgumentParser(description='Generate pingall mininet script')
 parser.add_argument('--numswitches', help='Number of switches',
                     type=int, action="store", default=3)
 parser.add_argument('--numhosts', help='Number of hosts',
-                    type=int, action="store", default=24)
+                    type=int, action="store")
+parser.add_argument('--hostsperswitch', help='Number of hosts',
+                    type=int, action="store")
 parser.add_argument('--test', help='Name of test',
                     type=str, action="store", default="t0")
 
 args = parser.parse_args(sys.argv[1:])
 
-nb_hosts = args.numhosts
+if args.numhosts and args.hostsperswitch:
+  raise parser.error("--numhosts incompatible with --hostsperswitch")
+
+if not args.numhosts and not args.hostsperswitch:
+  raise parser.error("must supply either --numhosts or --hostsperswitch")
+
+if args.numhosts:
+  assert(args.numhosts % args.numswitches == 0)
+  nb_hosts = args.numhosts
+
+else: #args.hostsperswitch:
+  nb_hosts = args.hostsperswitch * args.numswitches
+
 nb_switches = args.numswitches
 assert(nb_hosts % nb_switches == 0)
 
--- tests/t03/gen_t03_arp_entries.py
+++ tests/t03/gen_t03_arp_entries.py
@@ -3,6 +3,8 @@
 import argparse
 import sys
 import random
+import os
+import code
 
 parser = argparse.ArgumentParser(description='Generate t03 table commands')
 parser.add_argument('--seed', help='Seed for pseudorandom numbers',
@@ -10,12 +12,26 @@ parser.add_argument('--seed', help='Seed for pseudorandom numbers',
 parser.add_argument('--numswitches', help='Number of switches',
                     type=int, action="store", default=3)
 parser.add_argument('--numhosts', help='Number of hosts',
-                    type=int, action="store", default=24)
+                    type=int, action="store")
+parser.add_argument('--hostsperswitch', help='Number of hosts',
+                    type=int, action="store")
 
 args = parser.parse_args(sys.argv[1:])
 
+if args.numhosts and args.hostsperswitch:
+  raise parser.error("--numhosts incompatible with --hostsperswitch")
+
+if not args.numhosts and not args.hostsperswitch:
+  raise parser.error("must supply either --numhosts or --hostsperswitch")
+
+if args.numhosts:
+  assert(args.numhosts % args.numswitches == 0)
+  nb_hosts = args.numhosts
+
+else: #args.hostsperswitch:
+  nb_hosts = args.hostsperswitch * args.numswitches
+
 nb_switches = args.numswitches
-nb_hosts = args.numhosts
 
 assert((nb_hosts % nb_switches) == 0)
 nb_hosts_per_switch = nb_hosts / nb_switches
@@ -24,6 +40,10 @@ nb_links_per_switch = nb_switches - 1 + nb_hosts_per_switch
 assert((nb_links_per_switch % 2) == 0)
 
 random.seed(args.seed)
+ip_addrs = range(1, 255)
+assigned_addrs = []
+for h in xrange(nb_hosts):
+  assigned_addrs.append(ip_addrs.pop(random.randint(0, len(ip_addrs) - 1)))
 
 for i in range(nb_switches):
   output = []
@@ -42,17 +62,22 @@ for i in range(nb_switches):
   output.append("table_set_default check_opcode send_packet")
 
   for j in range(1, nb_hosts+1):
-    hex_id = hex(j).split('x')[-1]
-    assert(len(hex_id) < 3)
-    if len(hex_id) < 2:
-      hex_id = '0' + hex_id
-    dest_ip = "0x0A0000" + hex_id
-    dest_mac = "0x0004000000" + hex_id
+    hex_macid = hex(j).split('x')[-1]
+    assert(len(hex_macid) < 3)
+    if len(hex_macid) < 2:
+      hex_macid = '0' + hex_macid
+    dest_mac = "0x0004000000" + hex_macid
+    hex_ipid = hex(assigned_addrs[j-1]).split('x')[-1]
+    assert(len(hex_ipid) < 3)
+    if len(hex_ipid) < 2:
+      hex_ipid = '0' + hex_ipid
+    dest_ip = "0x0A0000" + hex_ipid
     output.append("table_add handle_arp_request arp_reply " + dest_ip \
                   + " => " + dest_mac)
 
   output.append("table_set_default handle_arp_request send_packet")
 
-  with open("t03_arp_entries_s%d"%(i+1), 'w') as out:
+  fname = os.path.dirname(os.path.realpath(__file__)) + '/t03_arp_entries_s%d'%(i+1)
+  with open(fname, 'w') as out:
     for line in output:
       out.write(line + '\n')
