dcedb592fcc0e72b8918241d4ff76511ef9b75b3
David Hancock <dhancock@cs.utah.edu>
2017-10-30 15:18:00 -0600

    Update t06 (routers + firewalls)

--- tests/run_t06.sh
+++ tests/run_t06.sh
@@ -6,22 +6,22 @@ ADMIN="$(cat /tmp/pts_admin)"
 BMV2_CLI="$(cat /tmp/pts_bmv2_cli)"
 SLICEMGR="$(cat /tmp/pts_slice_manager)"
 
-ttyecho -n $MININET ./run.sh --commands hp4commands.txt --topo ~/hp4-ctrl/tests/t05/topo.txt
+ttyecho -n $MININET ./run.sh --commands hp4commands.txt --scenario chain --topo ~/hp4-ctrl/tests/t05/topo.txt
 
 ttyecho -n $CONTROLLER ./controller --debug
 
 # pause for keypress
-echo "Next: create/provision 'jupiter' slice with lease to 'alpha' physical device"
+echo "Next: create/provision 'jupiter' slice with lease to alpha and bravo"
 read -n 1 -s
 
 ttyecho -n $ADMIN ./client --debug --startup tests/t06/t06_admin_1 admin
 
-echo "Next: create and configure L3 router"
+echo "Next: update topology"
+read -n 1 -s
+
+ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t06/t06_changenodes
+
+echo "Next: create and configure L3 routers"
 read -n 1 -s
 
 ttyecho -n $SLICEMGR ./client --debug --startup tests/t06/t06_jupiter_1 jupiter
-
-echo "Next: pairpings"
-read -n 1 -s
-
-ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t01/t01_pairpings
