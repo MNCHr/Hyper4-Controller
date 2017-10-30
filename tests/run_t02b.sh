6083125a2b67983a7e9cfbdad0c8dca8362bd799
David Hancock <dhancock@cs.utah.edu>
2017-10-30 11:18:39 -0600

    Update tests

---
+++ tests/run_t02b.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+MININET="$(cat /tmp/pts_mininet)"
+CONTROLLER="$(cat /tmp/pts_controller)"
+ADMIN="$(cat /tmp/pts_admin)"
+BMV2_CLI="$(cat /tmp/pts_bmv2_cli)"
+SLICEMGR="$(cat /tmp/pts_slice_manager)"
+
+ttyecho -n $MININET ./run.sh --commands hp4commands.txt --topo ~/hp4-ctrl/tests/t02/topo.txt
+
+ttyecho -n $CONTROLLER ./controller --debug
+
+echo "Next: split subnet / change addresses"
+read -n 1 -s
+
+ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t02/t02_mininet
+
+echo "Next: create/provision 'jupiter' slice with lease to 'alpha' physical device"
+read -n 1 -s
+
+ttyecho -n $ADMIN ./client --debug --startup tests/t02/t02_admin admin
+ttyecho -n $SLICEMGR ./client --debug jupiter
+
+echo "Next: add firewall"
+read -n 1 -s
+
+ttyecho -n $SLICEMGR source /home/ubuntu/hp4-ctrl/tests/t02/t02b_add_firewall
+
+echo "Next: create and configure L3 router virtual devices"
+read -n 1 -s
+
+#ttyecho -n $SLICEMGR ./client --debug --startup tests/t02/t02_jupiter_b jupiter
+ttyecho -n $SLICEMGR source /home/ubuntu/hp4-ctrl/tests/t02/t02_jupiter_b
+
+echo "Next: pairpings"
+read -n 1 -s
+
+ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t02/t02_pairpings
