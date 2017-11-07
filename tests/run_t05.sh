2c95f6673f0ca72e6259aab5d99539cc94ca18ea
David Hancock <dhancock@cs.utah.edu>
2017-11-07 12:05:57 -0700

    Modify t05 to better achieve stated goals
    Topology has been changed, and two slices are now created.
    
    Topology now has 5 hosts on s1, 2 hosts on s2, and s1 still
    connected to s2 as before.
    
    Slice A ('jupiter'):
    - s1 interfaces connected to:
      -- h1, h2, h3
      -- s2
    - all s2 interfaces (connected to h6, h7, and s1)
    
    Slice B ('saturn'):
    - s1 interfaces connected to h4 and h5.
    
    This way, we can, say, start an iperf session between h1 and h2,
    while we try things out with h3 connecting to h6/h7 as functions
    are added/removed.  And we can also have a session between
    h4 and h5 and look at the impacts on Slice B while changes are
    made to Slice A.

--- tests/run_t05.sh
+++ tests/run_t05.sh
@@ -11,57 +11,31 @@ ttyecho -n $MININET ./run.sh --commands hp4commands.txt --scenario chain --topo 
 ttyecho -n $CONTROLLER ./controller --debug
 
 # pause for keypress
-echo "Next: create/provision 'jupiter' slice with lease to 'alpha' physical device"
+echo "Next: create/provision 'jupiter' slice with leases to alpha and bravo"
 read -n 1 -s
 
 ttyecho -n $ADMIN ./client --debug --startup tests/t05/t05_admin_1 admin
 
-echo "Next: create and configure L2 switch virtual device"
+echo "Next: create and configure L2 switch virtual devices"
 read -n 1 -s
 
 ttyecho -n $SLICEMGR ./client --debug --startup tests/t05/t05_jupiter_1 jupiter
 
-echo "Next: pairpings"
-read -n 1 -s
-
-ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t05/t05_pairpings
-
-#echo "Next: expand topology"
 echo "Next: update topology"
 read -n 1 -s
 
-#ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t05/t05_addnodes
 ttyecho -n $MININET source /home/ubuntu/hp4-ctrl/tests/t05/t05_changenodes
 
-echo "Next: provision 'jupiter' with lease to 'bravo' physical device"
+echo "Next: try pings. Expected connectivity domains: (h1 h2 h3), (h6 h7)"
 read -n 1 -s
 
-ttyecho -n $ADMIN source tests/t05/t05_admin_2
-
-echo "Next: create and configure L2 switch vdev for bravo"
+echo "Next: manual xterm h1 h2 h3 h4 h5 h6"
 read -n 1 -s
 
-ttyecho -n $SLICEMGR source tests/t05/t05_jupiter_2
-
-echo "Observe: pings between subnets don't work (no L3 router function)"
-read -n 1 -s
-
-ttyecho -n $MININET h1 ping h4 -c 1 -W 1 &
-sleep 2
-ttyecho -n $MININET h2 ping h5 -c 1 -W 1 &
-sleep 2
-ttyecho -n $MININET h3 ping h6 -c 1 -W 1 &
-
-echo "Next: xterm h1 h2 h3 h4 h5 h6"
-read -n 1 -s
-#ttyecho -n $MININET "h1 xterm -hold -e \"bash -c \"tty > /tmp/pts_h1; exec bash\"\" &"
-#ttyecho -n $MININET h1 xterm -e "bash -c \"tty > /tmp/pts_h1; exec bash\"" \&
-#echo "press key to continue"
-#read -n 1 -s
-#ttyecho -n $MININET h2 xterm -e "bash -c \"tty > /tmp/pts_h2; exec bash\"" \&
 echo "Next: manually start iperf h1 -> h2"
 read -n 1 -s
-echo "Next: manually start pings h3 -> h6: ping 10.2.0.106"
+
+echo "Next: manually start pings h2 -> h6: ping 10.2.0.106"
 read -n 1 -s
 #H1="$(cat /tmp/pts_h1)"
 #H2="$(cat /tmp/pts_h2)"
@@ -75,10 +49,9 @@ read -n 1 -s
 echo "Next: add L3 router to alpha and bravo"
 read -n 1 -s
 
-ttyecho -n $SLICEMGR source tests/t05/t05_jupiter_3
+ttyecho -n $SLICEMGR source tests/t05/t05_jupiter_2
 
-#ttyecho -n $MININET h1 ping h4 -c 1 -W 1 &
-#sleep 2
-#ttyecho -n $MININET h2 ping h5 -c 1 -W 1 &
-#sleep 2
-#ttyecho -n $MININET h3 ping h6 -c 1 -W 1 &
+echo "Next: try pings. Expected connectivity domains: (h1 h2 h3 h6 h7)"
+read -n 1 -s
+
+
