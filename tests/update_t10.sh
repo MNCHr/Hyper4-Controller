7c747da2349d705728548553346df9ad1622b661
David Hancock <dhancock@cs.utah.edu>
2018-09-19 21:09:56 -0600

    Fix path bug in update_t10.sh (a vibrant eval script)

--- tests/update_t10.sh
+++ tests/update_t10.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-source t10/infr_manifest.sh
+t10path="tests/t10"
 
-sudo cp t10/t10_admin_orig t10/t10_admin
+source $t10path/infr_manifest.sh
 
-sudo sed -i "s/s1 <ip> <port>/s1 $s1_ip 9090/g" t10/t10_admin
-sudo sed -i "s/s2 <ip> <port>/s2 $s2_ip 9090/g" t10/t10_admin
-sudo sed -i "s/s3 <ip> <port>/s3 $s3_ip 9090/g" t10/t10_admin
-sudo sed -i "s/s4 <ip> <port>/s4 $s4_ip 9090/g" t10/t10_admin
+sudo cp $t10path/t10_admin_orig $t10path/t10_admin
+
+sudo sed -i "s/s1 <ip> <port>/s1 $s1_ip 9090/g" $t10path/t10_admin
+sudo sed -i "s/s2 <ip> <port>/s2 $s2_ip 9090/g" $t10path/t10_admin
+sudo sed -i "s/s3 <ip> <port>/s3 $s3_ip 9090/g" $t10path/t10_admin
+sudo sed -i "s/s4 <ip> <port>/s4 $s4_ip 9090/g" $t10path/t10_admin
